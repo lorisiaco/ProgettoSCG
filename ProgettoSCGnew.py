a4634eda94958328c9d2e2bc534ed4133cb3bdd4
lorisiaco <loris.iacoban@gmail.com>
2024-05-02 13:20:23 +0200

    Giusto Adesso

---
+++ ProgettoSCGnew.py
@@ -0,0 +1,186 @@
+from flask import Flask, render_template
+import sqlite3
+import Prova
+import webbrowser
+
+
+app = Flask(__name__)
+webbrowser.open("http://127.0.0.1:5000")
+
+@app.route('/')
+def table():
+    Prova.CreazioneDB()
+    Prova.CreaViste()
+    conn = sqlite3.connect("Database.db")
+    cur = conn.cursor()
+    cur.execute('SELECT ROUND(sum(TotaleCostiBudget),2) FROM TotaleCostiBudgetDEFINITIVO ')
+    risultat1 = cur.fetchall()
+    risultati1 = list(map(float, [x[0] for x in risultat1]))
+        # Query 2
+    cur.execute('SELECT ROUND( sum(TotaleCostiConsuntivo),2) FROM TotaleCostiConsuntivoDEFINITIVO')
+    risultat2 = cur.fetchall()
+    risultati2 = list(map(float, [x[0] for x in risultat2]))
+    #RICAVI
+        # Query 3
+    cur.execute('SELECT ROUND(TotaleVenditeBudget,2) FROM totalevenditebudget')
+    risultat3 = cur.fetchall()
+    risultati3 = list(map(float, [x[0] for x in risultat3]))
+        # Query 4
+    cur.execute('SELECT ROUND(TotaleVenditeConsuntivo,2) FROM totalevenditeconsuntivo')
+    risultat4 = cur.fetchall()
+    risultati4 = list(map(float, [x[0] for x in risultat4]))
+    #MIX STANDARD
+    # Query 5
+    cur.execute('SELECT ROUND(sum(venditemix),2) from  mixstandardvendite')
+    risultat5 = cur.fetchall()
+    risultati5 = list(map(float, [x[0] for x in risultat5]))
+    # Query 6
+    cur.execute('SELECT ROUND(sum(TotaleCostiMixStandard),2) from  TotaleCostiMixStandard')
+    risultat6 = cur.fetchall()
+    risultati6 = list(map(float, [x[0] for x in risultat6]))
+    # Query 7
+    cur.execute('SELECT ROUND(sum(MOL_mix_standard),2) from  molmixstandard')
+    risultat7 = cur.fetchall()
+    risultati7 = list(map(float, [x[0] for x in risultat7]))
+    #MIX EFFETTIVO
+    # Query 8
+    cur.execute('SELECT ROUND(sum(venditemix),2) from  mixeffettivovendite')
+    risultat8 = cur.fetchall()
+    risultati8 = list(map(float, [x[0] for x in risultat8]))
+    # Query 9
+    cur.execute('SELECT ROUND(sum(TotaleCostiMixEffettivo),2) from  TotaleCostiMixEffettivo')
+    risultat9 = cur.fetchall()
+    risultati9 = list(map(float, [x[0] for x in risultat9]))
+    # Query 10
+    cur.execute('SELECT ROUND(sum(MOL_mix_effettivo),2) from  molmixeffettivo')
+    risultat10 = cur.fetchall()
+    risultati10 = list(map(float, [x[0] for x in risultat10]))
+    #MOL
+    #Query 11
+    cur.execute("SELECT ROUND(sum(MOL_BUDGET),2) from molbudget")
+    risultat11 = cur.fetchall()
+    risultati11 = list(map(float, [x[0] for x in risultat11]))
+    #Query 12
+    cur.execute("SELECT ROUND(sum(MOL_Consuntivo),2) from molconsuntivo")
+    risultat12 = cur.fetchall()
+    risultati12 = list(map(float, [x[0] for x in risultat12]))
+    #Quantitá
+    #Query 13
+    cur.execute("SELECT ROUND(sum(Qta),0) FROM VolumiArticoliBudget ")
+    risultat13 = cur.fetchall()
+    risultati13 = list(map(int, [x[0] for x in risultat13]))
+    #Query 14
+    cur.execute("SELECT ROUND(sum(Qta),0) FROM VolumiArticoliConsuntivo ")
+    risultat14 = cur.fetchall()
+    risultati14 = list(map(int, [x[0] for x in risultat14]))
+    #Query 15
+    cur.execute("SELECT ROUND(sum(qta),0) FROM volTotaleMixStandard ")
+    risultat15 = cur.fetchall()
+    risultati15 = list(map(int, [x[0] for x in risultat15]))
+    #Query 16
+    cur.execute("SELECT ROUND(sum(qta),0) FROM volTotaleMixconsuntivo ")
+    risultat16 = cur.fetchall()
+    risultati16 = list(map(int, [x[0] for x in risultat16]))
+    #DELTA
+    #Delta1
+    cur.execute("SELECT ROUND(sum(venditemix),2)-(SELECT ROUND(TotaleVenditeBudget,2) FROM totalevenditebudget) from  mixstandardvendite ")
+    Delt1=cur.fetchall()
+    Delta1 = [round(x[0], 2) for x in Delt1]
+    #Delta2
+    cur.execute("SELECT ROUND(sum(venditemix),2)-(SELECT ROUND(sum(venditemix),2) from  mixstandardvendite) from  mixeffettivovendite")
+    Delt2=cur.fetchall()
+    Delta2 = [round(x[0], 2) for x in Delt2]
+    #Delta3
+    cur.execute("SELECT ROUND(TotaleVenditeConsuntivo,2)-(SELECT ROUND(sum(venditemix),2) from  mixeffettivovendite) FROM totalevenditeconsuntivo")
+    Delt3=cur.fetchall()
+    Delta3 = [round(x[0], 2) for x in Delt3]
+    #Delta4
+    cur.execute(" SELECT ROUND(sum(TotaleCostiMixStandard),2) -(SELECT ROUND(TotaleCostiBudget,2) FROM TotaleCostiBudgetDEFINITIVO ) from  TotaleCostiMixStandard")
+    Delt4=cur.fetchall()
+    Delta4 = [round(x[0], 2) for x in Delt4]
+    #Delta5
+    cur.execute("SELECT ROUND(sum(TotaleCostiMixEffettivo),2)-(SELECT ROUND(sum(TotaleCostiMixStandard),2) from  TotaleCostiMixStandard) from  TotaleCostiMixEffettivo")
+    Delt5=cur.fetchall()
+    Delta5 = [round(x[0], 2) for x in Delt5]
+    #Delta6
+    cur.execute("SELECT ROUND(TotaleCostiConsuntivo,2) -(SELECT ROUND(sum(TotaleCostiMixEffettivo),2) from  TotaleCostiMixEffettivo)FROM TotaleCostiConsuntivoDEFINITIVO")
+    Delt6=cur.fetchall()
+    Delta6 = [round(x[0], 2) for x in Delt6]
+    #Delta7
+    cur.execute("SELECT ROUND(sum(MOL_mix_standard),2) -(SELECT ROUND(sum(MOL_BUDGET),2) from molbudget)from  molmixstandard")
+    Delt7=cur.fetchall()
+    Delta7 = [round(x[0], 2) for x in Delt7]
+    #Delta8
+    cur.execute("SELECT ROUND(sum(MOL_mix_effettivo),2)-(SELECT ROUND(sum(MOL_mix_standard),2) from  molmixstandard) from  molmixeffettivo")
+    Delt8=cur.fetchall()
+    Delta8 = [round(x[0], 2) for x in Delt8]
+    #Delta9
+    cur.execute("SELECT ROUND(sum(MOL_Consuntivo),2) -(SELECT ROUND(sum(MOL_mix_effettivo),2) from  molmixeffettivo)from molconsuntivo")
+    Delt9=cur.fetchall()
+    Delta9 = [round(x[0], 2) for x in Delt9]
+    #Delta10
+    cur.execute("SELECT ROUND(sum(qta),0)-(SELECT ROUND(sum(Qta),0) FROM VolumiArticoliBudget) FROM volTotaleMixStandard")
+    Delt10=cur.fetchall()
+    Delta10 = [int(x[0]) for x in Delt10]
+    #Delta11
+    cur.execute("SELECT ROUND(sum(qta),0)-(SELECT ROUND(sum(qta),0) FROM volTotaleMixStandard) FROM volTotaleMixconsuntivo ")
+    Delt11=cur.fetchall()
+    Delta11 = [int(x[0]) for x in Delt11]
+    #Delta12
+    cur.execute("SELECT ROUND(sum(Qta),0) -(SELECT ROUND(sum(qta),0) FROM volTotaleMixconsuntivo )FROM VolumiArticoliConsuntivo")
+    Delt12=cur.fetchall()
+    Delta12 = [int(x[0]) for x in Delt12]
+    # chiusura della connessione
+    conn.close()
+
+    return render_template('home.html', risultati1=risultati1, risultati2=risultati2 ,risultati3=risultati3, risultati4=risultati4, risultati5=risultati5, risultati6=risultati6 , risultati7=risultati7 , risultati8=risultati8 , risultati9=risultati9, risultati10=risultati10 , risultati11=risultati11 , risultati12=risultati12 , risultati13=risultati13 , risultati14=risultati14 , risultati15=risultati15 , risultati16=risultati16 , Delta1=Delta1, Delta2=Delta2, Delta3=Delta3 , Delta4=Delta4, Delta5=Delta5 , Delta6=Delta6 , Delta7=Delta7 , Delta8=Delta8 , Delta9=Delta9, Delta10=Delta10, Delta11=Delta11 , Delta12=Delta12)
+
+@app.route('/TotaleVenditeBudget')  # valore unico 351708.8378556848
+def VALUTACONSUNTIVO():
+    conn = sqlite3.connect("Database.db")
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM TotaleVenditeBudgetVALUTACONSUNTIVO ') # valore unico 351708.8378556848
+    rs = cur.fetchall()
+    return render_template('TotaleVenditeBudget.html', rs=rs)
+
+@app.route('/ScostamentoTassiDiCambio')
+def ScostamentoTassiDiCambio():
+    conn = sqlite3.connect("Database.db")
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM ScostamentoTassiDiCambio ')
+    rs1 = cur.fetchall()
+    cur.execute('SELECT ROUND(TotaleVenditeBudgetVALUTACONSUNTIVO,2) FROM TotaleVenditeBudgetVALUTACONSUNTIVO   ')
+    rs4 = cur.fetchall()
+    rss4 = [float(x[0]) for x in rs4]
+    cur.execute('SELECT ROUND(TotaleVenditeConsuntivo,2) FROM totalevenditeconsuntivo')
+    r = cur.fetchall()
+    rr= list(map(float, [x[0] for x in r]))
+    cur.execute('SELECT ROUND(TotaleVenditeConsuntivo,2)-( SELECT ROUND(TotaleVenditeBudgetVALUTACONSUNTIVO,2) FROM TotaleVenditeBudgetVALUTACONSUNTIVO) FROM totalevenditeconsuntivo')
+    x1 = cur.fetchall()
+    xx= [round(x[0], 2) for x in x1]
+    return render_template('ScostamentoTassiDiCambio.html', rs1=rs1, rss4=rss4, rr=rr, xx=xx)
+
+@app.route('/ScostamentoCostoOrarioAreaProduzione')
+def ScostamentoCostoOrarioAreaProduzione():
+    conn = sqlite3.connect("Database.db")
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM ScostamentoCostoOrarioAreaProd  ')
+    rs2 = cur.fetchall()
+    return render_template('ScostamentoCostoOrarioAreaProduzione.html', rs2=rs2)
+
+@app.route('/ScostamentoMateriePrime')
+def ScostamentoMateriePrime():
+    conn = sqlite3.connect("Database.db")
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM ScostamentoMateriePrime   ')
+    rs3 = cur.fetchall()
+    return render_template('ScostamentoMateriePrime.html', rs3=rs3)
+
+@app.route('/ChiSiamo')
+def ChiSiamo():
+    return render_template('ChiSiamo.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+   
--- Prova.py
+++ Prova.py
@@ -3,7 +3,7 @@ import sqlite3
 def CreazioneDB():
     # connessione al database
     conn = sqlite3.connect('Database.db')
-     # lettura del file CSV
+    # lettura del file CSV
     TabellaClienti=pd.read_csv('Clienti.csv',index_col=False, delimiter = ',')
     Cli=TabellaClienti[["Nr.","Fatt. cumulative","Valuta"]]
     Cli=Cli.rename(columns={'Nr.': 'CodCli','Fatt. cumulative':'FattureCum'})
@@ -12,18 +12,14 @@ def CreazioneDB():
     TabellaVendite=pd.read_csv('Vendite1.csv',index_col=False, delimiter = ';')
     Ven=TabellaVendite[["Nr. movimento","budget/cons","Nr articolo","Nr. origine","Quantità","Importo vendita in valuta locale (TOTALE VENDITA)"]]
     Ven=Ven.rename(columns={'Nr. movimento': 'NrMov','budget/cons':'Tipologia','Nr articolo':'NrArtV','Nr. origine': 'NrOrig','Quantità':'Qta','Importo vendita in valuta locale (TOTALE VENDITA)':'ImportV'})
-
     TabellaConsumi=pd.read_csv('Consumi.csv',index_col=False, delimiter = ';')
     Cons=TabellaConsumi[["Nr. movimento","Budget/cons","Codice MP","Nr articolo","Nr. documento","Quantità MP impiegata","Importo costo (TOTALE)"]]
     Cons=Cons.rename(columns={'Nr. movimento': 'NrMovim','Budget/cons':'Tipol','Codice MP':'CodiceMP','Nr articolo': 'NrArtC','Nr. documento':'NrDoc','Quantità MP impiegata':'QuantitàMP','Importo costo (TOTALE)':'ImportoTot'})
-
     TabellaCostRis=pd.read_csv('CostoRisorse.csv',index_col=False, delimiter = ';')
     TabellaCostRis=TabellaCostRis.rename(columns={'IDRis': 'IDRis','Risorsa':'RisorsaR','Area di produzione': 'AreaProdR','Costo orario Budget(€/h)':'CostoBudgetR','Costo orario Cons (€/h)':'CostoConsR'})
-
     Imp=pd.read_csv('Impiego.csv',index_col=False, delimiter = ';')
     Imp=Imp.rename(columns={'IDImp': 'IDImp','nr articolo':'NrArt','budget/consuntivo':'Tipologia','Nr. Ordine di produzione': 'NrOrdine','Descrizione':'Descrizione','Nr. Area di produzione':'NrArea','Risorsa':'RisorsaC','Tempo risorsa':'Tempo','Quantità di output':'Quantitá'})
-
-    # scrittura dei dati del DataFrame in una tabella SQLite
+    # scrittura dei dati del DataFrame in una tabella SQLite facendo cosi tolgo il ciclo for ed ottimizzo il caricamento e la creazione del database ad un tempo quasi istante
     Cli.to_sql('Cliente', conn, if_exists='replace')
     TabellaTdC.to_sql('Valuta', conn, if_exists='replace')
     Ven.to_sql('Vendita', conn, if_exists='replace')
@@ -80,7 +76,6 @@ def CreaViste():
     cursor.execute('DROP VIEW IF EXISTS quantitàOutputConsuntivo;')
     cursor.execute('DROP VIEW IF EXISTS costounitarioConsuntivo;')
     cursor.execute('DROP VIEW IF EXISTS costoPerArticoloConsuntivo;')
-    cursor.execute('DROP VIEW IF EXISTS semilavorati;')
     cursor.execute('DROP VIEW IF EXISTS mixstandardVendite;')
     cursor.execute('DROP VIEW IF EXISTS mixeffettivovendite;')
     cursor.execute('DROP VIEW IF EXISTS ricavimixstandard;')
@@ -94,6 +89,42 @@ def CreaViste():
     cursor.execute('DROP VIEW IF EXISTS molconsuntivo;')
     cursor.execute('DROP VIEW IF EXISTS molmixstandard;')
     cursor.execute('DROP VIEW IF EXISTS molmixeffettivo;')
+    #PARTE NUOVA/QUERY NUOVE PER MODIFICA
+    cursor.execute('DROP VIEW IF EXISTS CostoImpiegoRisorseBUDGET;')
+    cursor.execute('DROP VIEW IF EXISTS CostiConsumiBudget;')
+    cursor.execute('DROP VIEW IF EXISTS CostiProdBudget;')
+    cursor.execute('DROP VIEW IF EXISTS CostiTotaliBudget;')
+    cursor.execute('DROP VIEW IF EXISTS CostiTotaliperArticoloBudget;')
+    cursor.execute('DROP VIEW IF EXISTS TotaleCostiBudgetDEFINITIVO')
+    cursor.execute('DROP VIEW IF EXISTS TotaleCostiConsuntivoDEFINITIVO;')
+    
+    cursor.execute('DROP VIEW IF EXISTS CostoImpiegoRisorseCONSUNTIVO;')
+    cursor.execute('DROP VIEW IF EXISTS CostiConsumiConsuntivo;')
+    cursor.execute('DROP VIEW IF EXISTS CostiProdConsuntivo;')
+    cursor.execute('DROP VIEW IF EXISTS CostiTotaliConsuntivo;')
+    cursor.execute('DROP VIEW IF EXISTS CostiTotaliperArticoloConsuntivo;')
+
+    cursor.execute('DROP VIEW IF EXISTS costimixstandardQueryBudget;')
+    cursor.execute('DROP VIEW IF EXISTS TotaleCostiMixStandard;')
+    cursor.execute('DROP VIEW IF EXISTS costimixeffettivoQueryBudget;')
+    cursor.execute('DROP VIEW IF EXISTS TotaleCostiMixEffettivo;')
+
+    cursor.execute('DROP VIEW IF EXISTS venditeBudgetinVALUTALOCALECONSUNTIVO;')
+    cursor.execute('DROP VIEW IF EXISTS ImportiVenditePerArticoloBudgetVALUTACONSUNTIVO;')
+
+    cursor.execute('DROP VIEW IF EXISTS TotaleVenditeBudgetVALUTACONSUNTIVO;')
+
+    cursor.execute('DROP VIEW IF EXISTS TassiBudget;')
+    cursor.execute('DROP VIEW IF EXISTS TassiConsuntivo;')
+
+    cursor.execute('DROP VIEW IF EXISTS ScostamentoTassiDiCambio ;')
+
+    cursor.execute('DROP VIEW IF EXISTS ScostamentoCostoOrarioAreaProd ;')
+
+    cursor.execute('DROP VIEW IF EXISTS costomedioMPperCodiceMPBudget ;')
+    cursor.execute('DROP VIEW IF EXISTS costomedioMPperCodiceMPConsuntivo ;')
+    cursor.execute('DROP VIEW IF EXISTS ScostamentoMateriePrime ;')
+
     cursor.execute("CREATE VIEW venditeBudget as select * from vendita where Tipologia='BUDGET' or Tipologia='Budget' and ImportV>0;")
     cursor.execute("CREATE VIEW venditeBudgetinVALUTALOCALE as select vb.NrMov,vb.Tipologia,vb.NrArtV,vb.NrOrig,vb.Qta,vb.ImportV/v.Tasso as RealImportV,v.CodVal from venditebudget vb join cliente c on vb.NrOrig=c.CodCLi join valuta v on c.Valuta=v.CodVal where v.Tipo='Budget' or v.Tipo='BUDGET';" )
     cursor.execute("Create view ImportiVenditePerArticoloBudget AS select vbvl.NrArtV,sum(RealImportV) as ImportoTotArticolo,sum(vbvl.Qta) as Qta from venditebudgetinvalutalocale vbvl group by vbvl.NrArtV; ")
@@ -131,9 +162,8 @@ def CreaViste():
     cursor.execute("create view costounitarioBudget as select `TOTALE_COSTI_A_BUDGET`/quantitàOutputBudget as costoUnitario from quantitàOutputBudget,totalecostibudget;")
     cursor.execute("create view costoPerArticoloBudget as select NrArt,costoUnitario*quantitàOutput as costoPerArticolo from costoUnitarioBudget,costivariabilieqtabudget;")
     cursor.execute("create view quantitàOutputConsuntivo as select sum(quantitàOutput) as quantitàOutputConsuntivo from costivariabilieqtaConsuntivo;")
-    cursor.execute("create view costounitarioConsuntivo as select `TOTALE COSTI A CONSUNTIVO`/quantitàOutputConsuntivo as costoUnitario from quantitàOutputConsuntivo,totalecosticonsuntivo;")
+    cursor.execute("create view costounitarioConsuntivo as select `TOTALE_COSTI_A_CONSUNTIVO`/quantitàOutputConsuntivo as costoUnitario from quantitàOutputConsuntivo,totalecosticonsuntivo;")
     cursor.execute("create view costoPerArticoloConsuntivo as select NrArt,costoUnitario*quantitàOutput as costoPerArticolo from costounitarioConsuntivo,costivariabilieqtaConsuntivo;")
-    cursor.execute("create view semilavorati as select NrArt,sum(quantitàOutput) from costivariabilieqtabudget v join consumo c on v.NrArt=c.CodiceMP where c.Tipol='Budget' or c.Tipol='BUDGET' group by NrArt;")
     cursor.execute("create view mixstandardVendite as select m.NrArtV,(p.prezzomedio*m.qta) as venditemix from mixstandard m join prezzomedioperarticolobudget p on m.NrArtV=p.NrArtV;")
     cursor.execute("create view mixeffettivovendite as select m.NrArtV,(p.prezzomedio*m.qta) as venditemix from mixeffettivo m join prezzomedioperarticoloconsuntivo p on m.NrArtV=p.NrArtV;")
     cursor.execute("create view ricavimixstandard as select sum(venditemix) as 'Ricavo' from mixstandardvendite;")
@@ -142,12 +172,55 @@ def CreaViste():
     cursor.execute("create view costitotalimixstandard as select sum(costimixstandard) as costitotalimixstandard from costimixstandardperarticolo;")
     cursor.execute("create view costimixeffettivoperarticolo as select c.NrArt,sum((c.costoPerArticolo*m.mix)) as costimixeffettivo from costoperarticolobudget c join mixvolumiconsuntivo m on c.NrArt=m.NrArtV group by c.NrArt;")
     cursor.execute("create view costitotalimixeffettivo as select sum(costimixeffettivo) as costitotalimixeffettivo from costimixeffettivoperarticolo;")
-    cursor.execute("create VIEW molbudget AS SELECT v.TotaleVenditeBudget - c.`TOTALE_COSTI_A_BUDGET` AS `MOL_BUDGET` FROM (totalecostibudget c JOIN totalevenditebudget v);")
-    cursor.execute("create view molconsuntivo AS SELECT (v.TotaleVenditeConsuntivo - c.`TOTALE_COSTI_A_CONSUNTIVO`) AS `MOL_CONSUNTIVO` FROM (totalecosticonsuntivo c JOIN totalevenditeconsuntivo v)    ; ")
-    cursor.execute("create view molmixstandard as select Ricavo-costitotalimixstandard as `MOL_mix_standard` from ricavimixstandard,costitotalimixstandard;")
-    cursor.execute("create view molmixeffettivo as select Ricavo-costitotalimixeffettivo as `MOL_mix_effettivo` from ricavimixeffettivo,costitotalimixeffettivo;")
+
+    #PARTE NUOVA/QUERY NUOVE PER MODIFICA
+    #NUOVI COSTI
+    #--BUDGET--
+    cursor.execute("Create view CostoImpiegoRisorseBUDGET as select i.NrArt,sum(i.Tempo*r.CostoBudgetR)as CostiImpiego from impiegoris i join risorsa r on (i.RisorsaC=r.RisorsaR and i.NrArea=r.AreaProdR ) where i.Tipologia like 'budget' group by i.NrArt;")
+    cursor.execute("create view CostiConsumiBudget as select NrArtC,sum(ImportoTot) as CostoConsumo from consumo where Tipol='Budget' or Tipol='BUDGET' group by NrArtC;")
+    cursor.execute("create view CostiProdBudget as select c.NrArtC,CostoConsumo+i.CostiImpiego as CostoTotaleProduzione from CostiConsumiBudget c join CostoImpiegoRisorseBUDGET i  on(c.NrArtC=i.NrArt) group by c.NrArtC;")
+    cursor.execute("create view CostiTotaliBudget as select cp.NrArtC,CostoTotaleProduzione/vb.Qta as CostoTotaleUnitario from CostiProdBudget cp join volumiarticolibudget vb on (cp.NrArtC=vb.NrArtV);")
+    cursor.execute("create view CostiTotaliperArticoloBudget as select cp.NrArtC,cp.CostoTotaleUnitario*vb.Qta as CostoTotalePerArticolo,vb.qta from CostiTotaliBudget cp join volumiarticolibudget vb on (cp.NrArtC=vb.NrArtV);")
+    #--CONSUNTIVO--
+    cursor.execute("Create view CostoImpiegoRisorseConsuntivo as select i.NrArt,sum(i.Tempo*r.CostoConsR)as CostiImpiego from impiegoris i join risorsa r on (i.RisorsaC=r.RisorsaR and i.NrArea=r.AreaProdR ) where i.Tipologia like 'Consuntivo' group by i.NrArt;")
+    cursor.execute("create view CostiConsumiConsuntivo as select NrArtC,sum(ImportoTot) as CostoConsumo from consumo where Tipol='Consuntivo' or Tipol='CONSUNTIVO' group by NrArtC;")
+    cursor.execute("create view CostiProdConsuntivo as select c.NrArtC,CostoConsumo+i.CostiImpiego as CostoTotaleProduzione from CostiConsumiConsuntivo c join CostoImpiegoRisorseConsuntivo i  on(c.NrArtC=i.NrArt) group by c.NrArtC;")
+    cursor.execute("create view CostiTotaliConsuntivo as select cp.NrArtC,CostoTotaleProduzione/vb.Qta as CostoTotaleUnitario from CostiProdConsuntivo cp join volumiarticoliConsuntivo vb on (cp.NrArtC=vb.NrArtV);")
+    cursor.execute("create view CostiTotaliperArticoloConsuntivo as select cp.NrArtC,cp.CostoTotaleUnitario*vb.Qta as CostoTotalePerArticolo,vb.qta from CostiTotaliConsuntivo cp join volumiarticoliConsuntivo vb on (cp.NrArtC=vb.NrArtV);")
+    #--CALCOLO COSTI MIX--
+    cursor.execute("create view costimixstandardQueryBudget as select c.NrArtC,sum(c.CostoTotaleUnitario*m.qta)as costimixstandard from CostiTotaliBudget c join mixstandard m on c.NrArtC=m.NrArtV group by c.NrArtC;")
+    cursor.execute("create view TotaleCostiMixStandard as select sum(c.costimixstandard) as TotaleCostiMixStandard from costimixstandardQueryBudget c;")
+    cursor.execute("create view costimixeffettivoQueryBudget as select c.NrArtC,sum(c.CostoTotaleUnitario*m.qta)as costimixeffettivo from CostiTotaliBudget c join mixeffettivo m on c.NrArtC=m.NrArtV group by c.NrArtC;")
+    cursor.execute("create view TotaleCostiMixEffettivo as select sum(c.costimixeffettivo) as TotaleCostiMixEffettivo from costimixeffettivoQueryBudget c;")
+
+    cursor.execute("CREATE VIEW venditeBudgetinVALUTALOCALECONSUNTIVO as select vb.NrMov,vb.Tipologia,vb.NrArtV,vb.NrOrig,vb.Qta,vb.ImportV/v.Tasso as RealImportV,v.CodVal from venditebudget vb join cliente c on vb.NrOrig=c.CodCLi join valuta v on c.Valuta=v.CodVal where v.Tipo = 'consuntivo' OR v.Tipo = 'CONSUNTIVO';")
+    cursor.execute("Create view ImportiVenditePerArticoloBudgetVALUTACONSUNTIVO AS select vbvl.NrArtV,sum(RealImportV) as ImportoTotArticolo,sum(vbvl.Qta) as Qta from venditeBudgetinVALUTALOCALECONSUNTIVO vbvl group by vbvl.NrArtV;")
+
+    cursor.execute("Create view TotaleVenditeBudgetVALUTACONSUNTIVO as select sum(ImportoTotArticolo) as TotaleVenditeBudgetValutaConsuntivo from ImportiVenditePerArticoloBudgetVALUTACONSUNTIVO;")
+
+    cursor.execute('create view TassiBudget as select CodVal,Tasso from valuta where Tipo="Budget" or Tipo="BUDGET";')
+    cursor.execute('create view TassiConsuntivo as select CodVal,Tasso from valuta where Tipo="Consuntivo" or Tipo="CONSUNTIVO";')
+
+    cursor.execute('create view ScostamentoTassiDiCambio as select b.Tasso as "TassoBudget", c.Tasso-b.Tasso as "Scostamento",c.Tasso as "TassoConsuntivo" from TassiBudget b,TassiConsuntivo c where b.CodVal=c.CodVal;')
+
+    cursor.execute('create view ScostamentoCostoOrarioAreaProd as select AreaProdR,RisorsaR,CostoBudgetR,(CostoConsR-CostoBudgetR) as Scostamento,CostoConsR from risorsa;')
+
+    cursor.execute('create view costomedioMPperCodiceMPBudget as select CodiceMP as CodiceMP, avg(ImportoTot/QuantitàMP) as costomedioMP from consumo where Tipol="Budget" or Tipol="BUDGET" group by CodiceMP;')
+    cursor.execute('create view costomedioMPperCodiceMPConsuntivo as select CodiceMP as CodiceMP, avg(ImportoTot/QuantitàMP) as costomedioMP from consumo where Tipol="Consuntivo" or Tipol="CONSUNTIVO" group by CodiceMP;')
+    cursor.execute('create view ScostamentoMateriePrime as select b.CodiceMP,b.costomedioMP as costomedioMPbudget,c.costomedioMP-b.costomedioMP as scostamento,c.costomedioMP as costomedioMPconsuntivo from costomedioMPperCodiceMPBudget b join costomedioMPperCodiceMPConsuntivo c on(b.CodiceMP=c.CodiceMP) group by b.CodiceMP')
+   
+    cursor.execute('create view TotaleCostiBudgetDEFINITIVO as select sum(CostoTotalePerArticolo) as TotaleCostiBudget from CostiTotaliperArticoloBudget;')
+    cursor.execute('create view TotaleCostiConsuntivoDEFINITIVO as select sum(CostoTotalePerArticolo) as TotaleCostiConsuntivo from CostiTotaliperArticoloConsuntivo;')
+
+    #--MOL--
+    cursor.execute("create VIEW molbudget AS SELECT v.TotaleVenditeBudget - c.TotaleCostiBudget AS `MOL_BUDGET` FROM (TotaleCostiBudgetDEFINITIVO c JOIN totalevenditebudget v);")
+    cursor.execute("create view molconsuntivo AS SELECT (v.TotaleVenditeConsuntivo - c.TotaleCostiConsuntivo) AS `MOL_CONSUNTIVO` FROM (TotaleCostiConsuntivoDEFINITIVO c JOIN totalevenditeconsuntivo v)    ; ")
+    cursor.execute("create view molmixstandard as select Ricavo-TotaleCostiMixStandard as `MOL_mix_standard` from ricavimixstandard,TotaleCostiMixStandard;")
+    cursor.execute("create view molmixeffettivo as select Ricavo-TotaleCostiMixEffettivo as `MOL_mix_effettivo` from ricavimixeffettivo,TotaleCostiMixEffettivo;")
+
     print("View created!")
     conn.close()
+    
  
 
 def Risultati():
